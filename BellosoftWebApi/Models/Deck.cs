04594ac0eab328b7070ad028ef18b7d6acd41d95
EZaca <ezaca@users.noreply.github.com>
2025-07-07 18:56:53 -0300

    Add initial database entities User and Deck
    - Add User class
    - Add Deck class

---
+++ BellosoftWebApi/Models/Deck.cs
@@ -0,0 +1,25 @@
+﻿namespace BellosoftWebApi.Models
+{
+    public class Deck
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int ExternalName { get; set; }
+        public int RemainingCards { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+
+        public Deck() { }
+
+        public Deck(int userId, int externalName, int remainingCards)
+        {
+            UserId = userId;
+            ExternalName = externalName;
+            RemainingCards = remainingCards;
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
