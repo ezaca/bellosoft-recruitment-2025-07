1fbc86bbfd3f81d1d5a392ff335fc8e1ed11d55e
EZaca <ezaca@users.noreply.github.com>
2025-07-09 17:32:21 -0300

    Add DeckController
    - Working version of DeckController
    - Inlude custom Responses for the application
    - Add /api/deck/list endpoint
    - Add /api/deck/select endpoint
    - Add /api/deck/create endpoint
    - Add /api/deck/delete endpoint
    - Add /api/deck/draw endpoint

---
+++ BellosoftWebApi/Responses/DeckResponse.cs
@@ -0,0 +1,19 @@
+﻿using BellosoftWebApi.Services.Sqids;
+
+namespace BellosoftWebApi.Responses
+{
+    public class DeckResponse
+    {
+        public string DeckId { get; set; }
+        public int RemainingCards { get; set; }
+
+        public DeckResponse(string deckId, int remainingCards)
+        {
+            DeckId = deckId;
+            RemainingCards = remainingCards;
+        }
+
+        public DeckResponse(ISqidsGenerator sqids, int deckDatabaseId, int remainingCards)
+            : this(sqids.Encode(deckDatabaseId), remainingCards) { }
+    }
+}
