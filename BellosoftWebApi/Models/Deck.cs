2843a7ce6422cc20cf316e7acf513dcd7b114c4f
EZaca <ezaca@users.noreply.github.com>
2025-07-08 22:15:38 -0300

    Fix field in Deck model and update migrations
    - Change ExternalName from int to string
    - Require 'dotnet ef database update'

--- BellosoftWebApi/Models/Deck.cs
+++ BellosoftWebApi/Models/Deck.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
-        public int ExternalName { get; set; }
+        public string ExternalName { get; set; }
         public int RemainingCards { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
@@ -13,7 +13,7 @@
 
         public Deck() { }
 
-        public Deck(int userId, int externalName, int remainingCards)
+        public Deck(int userId, string externalName, int remainingCards)
         {
             UserId = userId;
             ExternalName = externalName;
