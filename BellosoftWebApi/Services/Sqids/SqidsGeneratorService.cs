10615abec4eadcb1267bbeed68b018f177ef8791
EZaca <ezaca@users.noreply.github.com>
2025-07-09 17:29:52 -0300

    Add random alphabet to Sqids
    - Improve security with a random alphabet
    - Notice Sqids is not used for heavy security purposes, it is more a dissuasion tool, it has its flaws and its fine

--- BellosoftWebApi/Services/Sqids/SqidsGeneratorService.cs
+++ BellosoftWebApi/Services/Sqids/SqidsGeneratorService.cs
@@ -6,12 +6,15 @@ namespace BellosoftWebApi.Services.Sqids
     {
         private readonly SqidsEncoder<int> encoder;
 
-        public SqidsGeneratorService()
+        public SqidsGeneratorService(IConfiguration configuration)
         {
+            string alphabet = configuration["Sqids:Alphabet"]
+                ?? throw new InvalidDataException("Key not configured for ID hashing algorithn");
+
             var options = new SqidsOptions()
             {
-                Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz",
-                MinLength = 8,
+                Alphabet = alphabet,
+                MinLength = 8, // Hard-coded because it is hard-coded in the routes too, no meaning in being configurable
             };
             encoder = new SqidsEncoder<int>(options);
         }
