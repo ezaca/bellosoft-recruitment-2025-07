34e6035a8a1f5b18277d78564c9c859dd17a0d2e
EZaca <ezaca@users.noreply.github.com>
2025-07-09 18:36:09 -0300

    Add ReportsController
    - Add controller
    - Add SQL version, working and equivalent to LINQ
    - Add LINQ version, working and equivalent to SQL
    - Add ActiveDeckResponse

---
+++ BellosoftWebApi/Responses/ActiveDeckResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace BellosoftWebApi.Responses
+{
+    public class ActiveDeckResponse
+    {
+        public string ExternalName { get; set; }
+        public DateTime DeckCreatedAt { get; set; }
+        public int RemainingCards { get; set; }
+        public string OwnerEmail { get; set; }
+        public bool IsSelected { get; set; }
+
+        public ActiveDeckResponse(string externalName, DateTime deckCreatedAt, int remainingCards, string ownerEmail, bool isSelected)
+        {
+            ExternalName = externalName;
+            DeckCreatedAt = deckCreatedAt;
+            RemainingCards = remainingCards;
+            OwnerEmail = ownerEmail;
+            IsSelected = isSelected;
+        }
+    }
+}
